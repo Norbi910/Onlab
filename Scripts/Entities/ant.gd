67d91a40c73894e30cd5f02c69f97352061b6d7d
Biró Norbert <bironorbika9@gmail.com>
2025-11-17 08:32:07 +0100

    death animation for ant

--- Scripts/Entities/ant.gd
+++ Scripts/Entities/ant.gd
@@ -3,20 +3,24 @@ extends CharacterBody2D
 @export var speed: float = 20
 
 var direction: int = 1
+var is_alive: bool = true
 @onready var ray_cast_ahead: RayCast2D = $%RayCastAhead
-@onready var animated_sprite: AnimatedSprite2D = $%AnimatedSprite2D
 @onready var ray_cast_down: RayCast2D = $%RayCastDown
+@onready var sprite: Sprite2D = $pivot/Sprite2D
 @onready var pivot: Node2D = $pivot
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
+	if not is_alive:
+		return 
 	if ray_cast_ahead.is_colliding() or not ray_cast_down.is_colliding():
 		turn_around()
 		
 	velocity.x = direction * speed 
 	
 	var ground_following_rotation: float= get_real_velocity().angle_to(Vector2(direction, 0)) * -direction
-	animated_sprite.rotation = move_toward(animated_sprite.rotation, ground_following_rotation, 0.1)
+	sprite.rotation = move_toward(sprite.rotation, ground_following_rotation, 0.1)
 	move_and_slide()
 
 func turn_around():
@@ -25,4 +29,11 @@ func turn_around():
 
 func _on_health_changed(hp: float) -> void:
 	if hp <= 0:
-		queue_free()
+		_die()
+
+func _die():
+	is_alive = false
+	$HealthComponent.queue_free()
+	$pivot/HitBoxComponent.queue_free()
+	$pivot/AttackComponent.queue_free()
+	animation_player.play("death")
